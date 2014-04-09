e4186871ea63aad6c3dd636825ac6a56828f4be0
NoAvailableAlias <apek.blog@gmail.com>
2014-04-09 19:37:24 -0400

    added some codebase information

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -9,6 +9,20 @@
     GLFW 3.0.4 or >
     Glew 1.10 or >
     C++11
+
+    Current bugs:
+    Depending on the platform vertical bars appear
+        when the window is resized horrizontally? mystery?
+    General fixed width discrepancies between 32 / 64 bit architectures.
+
+    Enhancements:
+    Fixed resource management not being exception safe by using RAII.
+    Optimized CPU hotspots #2 and #3 by elminating unwanted copies.
+
+    Notes:
+    CodeXL reports search<>() has ~60% of CPU time.
+    Performance seems to be bound by CPU -> GPU transfer rates.
+    Preliminary results show bigalg<>() being slower than search<>().
 */
 
 int main()
