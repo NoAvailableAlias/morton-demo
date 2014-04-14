3830ee36d5f0457ee3705517adb29159659a1c29
NoAvailableAlias <apek.blog@gmail.com>
2014-04-13 21:22:13 -0400

    added shared_ptr wrapper

--- cpp/MortonDemo.hpp
+++ cpp/MortonDemo.hpp
@@ -1,20 +1,41 @@
 #ifndef MORTON_DEMO_HPP
 #define MORTON_DEMO_HPP
 
+#include "ShaderProgram.hpp"
 #include "DemoState.hpp"
 
+#include <sstream>
 #include <memory>
+
+struct GLFWwindow;
 
 // ApEk,
 // NoAvailableAlias
 // this code is public domain
 
-struct GLFWwindow;
-
 class MortonDemo
 {
+    class SharedExit
+    {
+        std::shared_ptr<SharedExit> finally;
+ 
+        public:
+ 
+        SharedExit() = default;
+ 
+        template <typename T> SharedExit(T&& lambda):
+            finally(this, std::forward<T>(lambda)) {}
+    };
+
+    std::stringstream log;
+    SharedExit finally;
     DemoState state;
-    std::shared_ptr<void> clean; // RAII cleanup code
+
+    ShaderProgram hatchAreaShader;
+    ShaderProgram searchedShader;
+
+    void drawHatchArea(ShaderProgram const&) const;
+    void drawSearched(ShaderProgram const&) const;
 
     void drawBackground() const;
     void drawHatchArea() const;
@@ -32,6 +53,8 @@ class MortonDemo
     static void cursorButtonCallback(GLFWwindow*, int, int, int);
     static void cursorPositionCallback(GLFWwindow*, double, double);
 
+    void writeLogFile();
+    void createShaders();
     void initializeDemo();
 
     public:
